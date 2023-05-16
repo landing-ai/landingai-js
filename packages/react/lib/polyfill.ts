c61f246799d1524a3183e1d2b754b1283b370f40
Lan Tian <tian.lan@landing.ai>
2023-05-16 17:44:32 +0800

    fix polyfill

--- packages/react/lib/polyfill.ts
+++ packages/react/lib/polyfill.ts
@@ -2,9 +2,9 @@
  * window.OffscreenCanvas
  * https://developer.mozilla.org/en-US/docs/Web/API/OffscreenCanvas
  */
-if (!self.OffscreenCanvas) {
+if (typeof window !== 'undefined' && !window.OffscreenCanvas) {
   // @ts-ignore
-  self.OffscreenCanvas = class OffscreenCanvas {
+  window.OffscreenCanvas = class OffscreenCanvas {
     canvas: any;
     constructor(width: number, height: number) {
       this.canvas = document.createElement('canvas');
