b08027402a8edca7faa49cd8661516971317e5db
Lan Tian <tian.lan@landing.ai>
2023-05-16 13:55:14 +0800

    export react

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -3,9 +3,3 @@ export * from './lib/utils/colorUtils';
 export * from './lib/utils/imageUtils';
 export * from './lib/utils/mathUtils';
 export * from './lib/types';
-
-export const testString = 'hello, world!';
-
-export default {
-  what: 'is the exported value?',
-};
--- packages/react/__tests__/test.test.ts
+++ packages/react/__tests__/test.test.ts
@@ -1,7 +1,5 @@
-import core, { palette, testString } from '@landingai-js/core';
+import { palette } from '@landingai-js/core';
 
 it('test importing core package', () => {
-  console.log('palette', palette);
-  console.log('testString', testString);
-  console.log('core', core);
+  expect(palette[0]).toEqual('#811776');
 });
