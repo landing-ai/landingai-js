e78fc95a58a69e2bdcbf13014156e2b69f7e9334
Lan Tian <tian.lan@landing.ai>
2023-05-25 16:01:03 +0800

    typedocs

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,6 +1,5 @@
 export * from './lib/utils/annotationUtils';
 export * from './lib/utils/colorUtils';
-export * from './lib/utils/imageUtils';
 export * from './lib/utils/mathUtils';
 export * from './lib/types';
 export * from './lib/api';
