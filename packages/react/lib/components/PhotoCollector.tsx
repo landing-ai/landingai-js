14aa6ba6588a4101532d08ca1de47c68c5138ad8
Lan Tian <tian.lan@landing.ai>
2023-06-08 10:47:15 +0800

    fix orientation issue

--- packages/react/lib/components/PhotoCollector.tsx
+++ packages/react/lib/components/PhotoCollector.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './index.module.css';
-import { EImageType, compressAccurately } from 'image-conversion';
+import { EImageType, compress } from 'image-conversion';
 
 export type PhotoCollectorProps = {
   setImage: (image: Blob) => void;
@@ -18,9 +18,10 @@ export const PhotoCollector: React.FC<PhotoCollectorProps> = (props) => {
     const files = (e.target as HTMLInputElement).files;
     if (files?.length && files[0]) {
       // Compress to remove EXIF information like orientation
-      const compressed = await compressAccurately(files[0], {
+      const compressed = await compress(files[0], {
         size: 1000,
         type: EImageType.JPEG,
+        quality: 0.9,
       });
       setImage(compressed);
     }
